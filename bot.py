5a1e19c57b1d7b1a4a3c66726b4eec4a94ab80ab
Art-025 <telegramuchun550@gmail.com>
2026-08-25 21:39:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from handlers.start import router as start_router
 from handlers.profile import router as profile_router
 from handlers.transfer import router as transfer_router
 from handlers.help import router as help_router
+from handlers.rules import router as rules_router
 
 # Logging
 logging.basicConfig(level=logging.INFO)
@@ -27,6 +28,7 @@ dp.include_router(start_router)
 dp.include_router(profile_router)
 dp.include_router(transfer_router)
 dp.include_router(help_router)
+dp.include_router(rules_router)
 
 
 async def main():
